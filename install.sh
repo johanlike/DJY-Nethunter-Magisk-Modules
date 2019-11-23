3bfa173b8c84f21213fed23ba197cddd649b8236
AlexeyZavar <alexeyzavar@gmail.com>
2019-11-23 17:27:50 +0300

    Minor script rewrite

--- install.sh
+++ install.sh
@@ -2,15 +2,8 @@ SKIPMOUNT=false
 PROPFILE=false
 POSTFSDATA=false
 LATESTARTSERVICE=false
-REPLACE_EXAMPLE="
-/system/app/Youtube
-/system/priv-app/SystemUI
-/system/priv-app/Settings
-/system/framework
-"
 REPLACE="
 "
-
 
 print_modname() {
   ui_print "- Nethunter module v68"
@@ -22,7 +15,10 @@ on_install() {
   ui_print "- Extracting module files"
   unzip -o "$ZIPFILE" 'system/*' -d "$MODPATH" >&2
   unzip -o "$ZIPFILE" 'apks/*' -d /sdcard/ >&2
-  unzip -o "$ZIPFILE" 'apks/*' -d /data/local/tmp/ >&2
+  mkdir -p /data/local/tmp/apks
+  cp -r -f /sdcard/apks/* /data/local/tmp/apks
+  ui_print "- Apk files extracted"
+
   ui_print "##################################################"
   ui_print "##                                              ##"
   ui_print "##  88      a8P         db        88        88  ##"
@@ -35,53 +31,57 @@ on_install() {
   ui_print "##  88       Y8b d8'          '8b 888888888 88  ##"
   ui_print "##                                              ##"
   ui_print "################### NetHunter ####################"
-  echo '- Installing apk files...'
+
+  ui_print '- Installing apk files...'
+
   apkDir="/data/local/tmp/apks/"
-  cd $apkDir || exit
 
-  function readDir(){
+  cd $apkDir || cleanup
+  installAll $apkDir $apkDir
 
-    cd $apkDir || exit
+  ui_print "- Pushing obb files..."
+  cp -r -f /sdcard/Android/obbs/* /sdcard/Android/obb/
 
-    filelist=$(ls "$1")
-
-    for file in $filelist
-
-    do
-
-        installApk "$file"
-
-    done
-
-  }
-
-  function installApk(){
-
-    file=$1
-
-    extension="${file##*.}"
-
-    if [ "$extension" = "apk" ]
-
-    then
-
-        echo "- Installing ""$file""..."
-        cp -r -f  /sdcard/apks/*.apk /data/local/tmp/
-        pm install "$file"
-
-    else
-
-        echo "- Error: ""$file" "is not an apk file."
-
-    fi
-
-  }
-
-  
-  readDir $apkDir
-  rm -rf /data/local/tmp/*
+  cleanup
 }
 
 set_permissions() {
   set_perm_recursive "$MODPATH" 0 0 0755 0644
+}
+
+installAll() {
+  cd "$2" || cleanup
+
+  filelist=$(ls "$1")
+  ui_print "- Apks that will be installed: $filelist" | tr "\n" " "
+  ui_print ""
+
+  for file in $filelist; do
+    installApk "$file"
+  done
+}
+
+installApk() {
+  file=$1
+
+  extension=${file##*.}
+
+  if [ "$extension" = "apk" ]; then
+    ui_print "- Installing $file..."
+    result=$(pm install "$file")
+
+    if [ "$result" = "Success" ]; then
+      ui_print "- Success on: $file"
+    else
+      ui_print "- Fail on: $file. Result was: $result"
+    fi
+  else
+    ui_print "- Error: $file is not an apk file."
+  fi
+}
+
+cleanup() {
+  ui_print "- Cleaning up..."
+  rm -rf /sdcard/Android/obbs
+  rm -rf /data/local/tmp/*
 }
